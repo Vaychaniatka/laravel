4019e1f5d9ebe0772fab670d003aa27391fff326
Vaychaniatka <solovei.pasha@gmail.com>
2016-09-02 11:34:07 +0300

    first working version

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,48 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(PostsSeeder::class);
+        $this->call(PostsTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
 
 
-class PostsSeeder extends Seeder
-{
-    public function run()
-    {
-        DB::table('posts')->delete();
-        Post::create([
-            'title'=>'First Post',
-            'slug'=>'First-Post',
-            'excerpt'=>'<b>First Post Body</b>',
-            'content'=>'<b>First Post Content</b>',
-            'published'=>true,
-            'published_at'=>Carbon::now(),
-
-        ]);Post::create([
-            'title'=>'Second Post',
-            'slug'=>'Second-Post',
-            'excerpt'=>'<b>Second Post Body</b>',
-            'content'=>'<b>Second Post Content</b>',
-            'published'=>true,
-            'published_at'=>Carbon::now(),
-
-        ]);Post::create([
-            'title'=>'Third Post',
-            'slug'=>'Third-Post',
-            'excerpt'=>'<b>Third Post Body</b>',
-            'content'=>'<b>Third Post Content</b>',
-            'published'=>true,
-            'published_at'=>Carbon::now(),
-
-        ]);
-    }
-
-}
-{
-
-}
 
 
 
